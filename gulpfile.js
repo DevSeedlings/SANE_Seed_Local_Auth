7ccab713e2528ff84212daf487e83c0e3912901a
Stephen Brinkworth <xoman5144@gmail.com>
2016-05-30 10:12:46 -0600

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var annotate = require('gulp-ng-annotate');
-var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
@@ -14,18 +13,17 @@ var paths = {
 
 gulp.task('js', function() {
   return gulp.src(paths.jsSource)
-  .pipe(plumber())
   .pipe(concat('bundle.js'))
   .pipe(annotate())
+  //.pipe(uglify()) //Uncomment when code is production ready
   .pipe(gulp.dest('./public'));
 });
 
 gulp.task('sass', function () {
   return gulp.src(paths.sassSource)
-    .pipe(sass({
-      paths: [ path.join(__dirname, 'styles') ]
-    }))
+    .pipe(sass())
     .pipe(concat('style.css'))
+    //.pipe(uglify()) //Uncomment when code is production ready
     .pipe(gulp.dest('./public/styles'));
 });
 
