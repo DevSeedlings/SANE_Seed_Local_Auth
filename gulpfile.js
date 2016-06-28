5b2242dc3bc5232fa6970f9706da32ecf8b41932
Stephen Brinkworth <sbrinkworth@Stephens-Air.home>
2016-06-27 21:19:09 -0600

    creating gulp-demo branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,45 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var annotate = require('gulp-ng-annotate');
-var uglify = require('gulp-uglify');
-var watch = require('gulp-watch');
-var sass = require('gulp-sass');
+// REQUIRE DEPENDENCIES
+// ============================================================
+// var gulp = require('gulp');
+// var concat = require('gulp-concat');
+// var annotate = require('gulp-ng-annotate');
+// var uglify = require('gulp-uglify');
+// var watch = require('gulp-watch');
+// var sass = require('gulp-sass');
+// var babel = require('gulp-babel');
 
-var paths = {
-  jsSource: ['./public/app/**/*.js', '!/public/bundle.js'],
-  sassSource: ['./public/styles/**/*.sass']
-};
+// DECLARE FILE PATHS
+// ============================================================
+// var paths = {
+//   jsSource: ['./public/app/**/*.js', '!/public/bundle.js'],
+//   sassSource: ['./public/styles/**/*.sass']
+// };
 
-gulp.task('js', function() {
-  return gulp.src(paths.jsSource)
-  .pipe(concat('bundle.js'))
-  .pipe(annotate())
-  //.pipe(uglify()) //Uncomment when code is production ready
-  .pipe(gulp.dest('./public'));
-});
+// DEFINE TASKS
+// ============================================================
+// gulp.task('js', function() {
+//   return gulp.src(paths.jsSource)
+//   //.pipe(babel()) //Uncomment if using ES6
+//   .pipe(concat('bundle.js'))
+//   .pipe(annotate())
+//   //.pipe(uglify()) //Uncomment when code is production ready
+//   .pipe(gulp.dest('./public'));
+// });
 
-gulp.task('sass', function () {
-  return gulp.src(paths.sassSource)
-    .pipe(sass())
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('./public/styles'));
-});
+// gulp.task('sass', function () {
+//   return gulp.src(paths.sassSource)
+//     .pipe(sass())
+//     .pipe(concat('style.css'))
+//     .pipe(gulp.dest('./public/styles'));
+// });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.jsSource, ['js']);
-  gulp.watch(paths.sassSource, ['sass']);
-});
+// WATCH TASKS
+// ============================================================
+// gulp.task('watch', function() {
+//   gulp.watch(paths.jsSource, ['js']);
+//   gulp.watch(paths.sassSource, ['sass']);
+// });
 
-gulp.task('default', ['watch', 'js', 'sass']);
+// RUN DEFAULT TASK - first thing to run when gulp is called
+// ============================================================
+// gulp.task('default', ['watch', 'js', 'sass']);
