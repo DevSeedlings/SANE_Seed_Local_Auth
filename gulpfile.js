dde79dbe119d701c308141e66ddc5c11c47fe3f5
Stephen Brinkworth <sbrinkworth@Stephens-Air.home>
2016-06-27 21:32:58 -0600

    updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,15 @@
 // var uglify = require('gulp-uglify');
 // var watch = require('gulp-watch');
 // var sass = require('gulp-sass');
+// //var less = require('gulp-less'); //Uncomment if using Less
 // var babel = require('gulp-babel');
 
 // DECLARE FILE PATHS
 // ============================================================
 // var paths = {
 //   jsSource: ['./public/app/**/*.js', '!/public/bundle.js'],
-//   sassSource: ['./public/styles/**/*.sass']
+//   sassSource: ['./public/styles/**/*.sass'], // Add to array or change current path to './public/styles/**/*.scss' to use Scss
+//   //lessSource: ['./public/styles/**/*.less'] //Uncomment if using Less
 // };
 
 // DEFINE TASKS
@@ -25,21 +27,29 @@
 //   //.pipe(uglify()) //Uncomment when code is production ready
 //   .pipe(gulp.dest('./public'));
 // });
-
+//
 // gulp.task('sass', function () {
 //   return gulp.src(paths.sassSource)
 //     .pipe(sass())
 //     .pipe(concat('style.css'))
-//     .pipe(gulp.dest('./public/styles'));
+//     .pipe(gulp.dest('./public'));
 // });
+//
+// // gulp.task('less', function () {       // Uncomment if using Less
+// //   return gulp.src(paths.lessSource)
+// //     .pipe(less())
+// //     .pipe(concat('style.css'))
+// //     .pipe(gulp.dest('./public'));
+// // });
 
 // WATCH TASKS
 // ============================================================
 // gulp.task('watch', function() {
 //   gulp.watch(paths.jsSource, ['js']);
 //   gulp.watch(paths.sassSource, ['sass']);
+// //   gulp.watch(paths.sassSource, ['sass']); //Uncomment if using Less
 // });
 
 // RUN DEFAULT TASK - first thing to run when gulp is called
 // ============================================================
-// gulp.task('default', ['watch', 'js', 'sass']);
+// gulp.task('default', ['watch', 'js', 'sass']); //Add 'sass' to array if using sass and less or replace 'sass' with 'less' if only using Less
