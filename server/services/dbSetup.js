5303655b57b5eb75c6ef4e5668d4a92ba92f9a1c
Kymbolde <kymboldexb@gmail.com>
2017-01-06 17:29:08 -0700

    Update dbSetup.js

--- server/services/dbSetup.js
+++ server/services/dbSetup.js
@@ -4,21 +4,21 @@ var db = app.get('db');
 var config = require('./../config');
 
 // ALLOW CONSOLE OUTPUT //
-var allowConsolePutput = config.INITALIZE_LOG;
+var allowConsolePutput = config.INITIALIZE_LOG;
 var log = function(input) {
 	if (allowConsolePutput) {
 		console.log(input);
 	}
 };
 
-// INITALIZE FUNCTION //
+// INITIALIZE FUNCTION //
 module.exports = {
 	run: function() {
 		console.log('Initializing database');
 
-		db.initalize.tables_initalize(function(err, table) {
+		db.initialize.tables_initialize(function(err, table) {
 			if (err) return log('Some tables failed to create');
-			else log('Tables successfully initalized');
+			else log('Tables successfully initialized');
 		});
 	}
 };
