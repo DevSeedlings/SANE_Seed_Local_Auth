4d5c7a16c2d19962dad27971f67d2f216b1382e6
Stephen Brinkworth <xoman5144@gmail.com>
2017-02-21 12:04:22 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var nodemon = require('gulp-nodemon');
 var paths = {
 	jsSource: ['public/app/**/*.js'],
 	sassSource: ['public/**/*.scss'],
+	htmlSource: ['public/**/*.html'],
 	server: ['server/index.js']
 };
 
@@ -20,19 +21,27 @@ gulp.task('sass', function() {
 	gulp.src(paths.sassSource)
 		.pipe(sass())
 		.pipe(concat('style.css'))
-		.pipe(gulp.dest('./public'));
+		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('js', function() {
 	gulp.src(paths.jsSource)
 		.pipe(annotate())
 		.pipe(concat('bundle.js'))
-		.pipe(gulp.dest('./public'));
+		.pipe(gulp.dest('./dist'));
 });
+
+gulp.task('html', function() {
+	gulp.src(paths.htmlSource)
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['js', 'sass', 'html']);
 
 gulp.task('watch', function() {
 	gulp.watch(paths.jsSource, ['js']);
 	gulp.watch(paths.sassSource, ['sass']);
+	gulp.watch(paths.htmlSource, ['html']);
 });
 
-gulp.task('default', ['js', 'sass', 'watch']);
+gulp.task('default', ['build', 'watch']); // add 'serve' to the array if you want gulp to run nodemon as well.
